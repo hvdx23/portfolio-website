640be0581d35bacf701c1e6469e1e081f9a137d0
hvdx23 <hv25@students.waikato.ac.nz>
2025-01-24 00:41:56 +1300

    landingpage div adjusted

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,22 +6,23 @@ import Footer from './Footer';
 
 function LandingPage() {
   return (
-
     <div className="landing-page">
-        <Header/>
-      
+      <Header />
+
       <h1>Welcome to My Portfolio</h1>
-      <img src={profileImage} alt="Profile" className="profile-image" />
-      <p>
-        Hi, I'm Harikrishnan Venugopal, a Data science enthusiast and Software Engineer. I am passionate about Data analysis, Data visualization and Web development in React.
-      </p>
-      <p>
-        Check out my work below and feel free to reach out!
-      </p>
-      <div className="projects">
-        {/* Add project components or links here */}
+      <div className="landing-page-content">
+        <img src={profileImage} alt="Profile" className="profile-image" />
+        <p>
+          Hi, I'm Harikrishnan Venugopal, a Data science enthusiast and Software
+          Engineer. I am passionate about Data analysis, Data visualization and
+          Web development in React.
+        </p>
+        <p>Check out my work below and feel free to reach out!</p>
+        <div className="projects">
+          <h1>PROJECTS</h1>
+        </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
