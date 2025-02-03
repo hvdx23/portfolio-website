7ec4da9c9cbd3c3b467413d928402f171b38f8e7
hvdx23 <hv25@students.waikato.ac.nz>
2025-02-03 20:18:19 +1300

    techstack icons added

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Techstack.css';
-import javaIcon from './assets/icons/java.png';
+// import javaIcon from './assets/icons/java.png';
 import Loader from './Loader';
 
 import StackIcon from "tech-stack-icons";
+
+const background = {
+  backgroundColor: 'yellow',
+  padding: '10px',
+  borderRadius: '5px'
+};
 
 function Techstack() {
   return (
@@ -17,9 +23,17 @@ function Techstack() {
       <div className="techstack">
         <StackIcon name="python" className="stack-icon" />
         <StackIcon name="reactjs" className="stack-icon" />
-        <img src={javaIcon} alt="Java" style={{ height: '100px', width: '100px' }} />
+        {/* <img src={javaIcon} alt="Java" style={{ height: '100px', width: '100px' }} /> */}
+        <StackIcon name="java" className="stack-icon" />
         <StackIcon name="js" className="stack-icon" />
         <StackIcon name="vuejs" className="stack-icon" />
+        <StackIcon name="nodejs" className="stack-icon" />
+        <StackIcon name="aws" className="stack-icon" style={background} />
+        <StackIcon name="azure" className="stack-icon" />
+        <StackIcon name="github" className="stack-icon" />
+        <StackIcon name="git" className="stack-icon" />
+
+
       </div>
     </div>
   );
