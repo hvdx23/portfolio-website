cbbdb585886793f10ae84eacade78c7803f30cd7
hvdx23 <hv25@students.waikato.ac.nz>
2025-02-10 14:20:37 +1300

    mobile version displays older repo vies. used usestate to dynamically show the new card in mobile and desktop versions

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from 'react';
-// import RepositoryCard from './RepositoryCard';
 import Card from './Repositoryver2Card';
+
 
 function Repositories() {
     const [repositories, setRepos] = useState([]);
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     useEffect(() => {
         fetch('https://api.github.com/users/hvdx23/repos')
             .then(response => response.json())
             .then(data => {
-            const updatedData = data.map(repo => {
-                if (repo.description === null) {
-                repo.description = "No description available";
-                }
-                return repo;
-            });
-            setRepos(updatedData);
+                const updatedData = data.map(repo => {
+                    if (repo.description === null) {
+                        repo.description = "No description available";
+                    }
+                    return repo;
+                });
+                setRepos(updatedData);
             })
             .catch(error => console.error('Error fetching repositories:', error));
     }, []);
@@ -23,11 +36,25 @@ function Repositories() {
     return (
         <div className="repositories">
             <h1>My Github Repositories</h1>
-            <div className="repo-list">
-                {repositories.map(repo => (
-                    <Card key={repo.id} repo={repo} />
-                ))}
-            </div>
+            {!isMobile && (
+                <div className="repo-list">
+                    {repositories.map(repo => (
+                        <Card key={repo.id} repo={repo} />
+                    ))}
+                </div>
+            )}
+            {isMobile && (
+                <div className="mobile-repo">
+                    <ul className="repo-box">
+                        {repositories.map(repo => (
+                            <li key={repo.id}>
+                                <a href={repo.html_url}>{repo.name}</a>
+                                <p style={{ textAlign: 'center', marginLeft: '10%', marginRight: '10%' }}>{repo.description}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
